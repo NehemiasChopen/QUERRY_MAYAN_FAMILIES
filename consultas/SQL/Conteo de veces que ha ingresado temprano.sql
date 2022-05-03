855103e6094d638198180ca27a23db9e591d78be
mfsistemas <sistemas@mayanfamilies.org>
2022-05-03 15:57:20 -0600

    Mejoras ya en la utilización.

--- consultas/SQL/Conteo de veces que ha ingresado temprano.sql
+++ consultas/SQL/Conteo de veces que ha ingresado temprano.sql
@@ -6,6 +6,6 @@ SELECT emp.cod_empleado,
     MAX(reg.hora_registro) AS 'hora_maxima',
     COUNT(reg.hora_registro) AS 'entradas_temprano'
 FROM tb_registro reg
-    INNER JOIN tb_empleado emp ON emp.id_empleado = reg.id_empleado
-WHERE reg.hora_registro BETWEEN '07:00' AND '09:05'
+    JOIN tb_empleado emp ON emp.id_empleado = reg.id_empleado
+WHERE reg.hora_registro < '09:05'
 GROUP BY emp.cod_empleado
